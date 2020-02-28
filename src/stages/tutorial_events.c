57f85898a5bc0680ad0b88135c4c1f3b6c342942
Alice D <alice@starwitch.productions>
2020-02-27 19:28:29 -0500

    tutorial stage cleanups

--- src/stages/tutorial_events.c
+++ src/stages/tutorial_events.c
@@ -14,55 +14,51 @@
 #include "common_tasks.h"
 
 TASK(burst_fairy, { cmplx pos; cmplx dir; }) {
-    Enemy *e = TASK_BIND_UNBOXED(create_enemy1c(ARGS.pos, 700, Fairy, NULL, ARGS.dir));
+	Enemy *e = TASK_BIND_UNBOXED(create_enemy1c(ARGS.pos, 700, Fairy, NULL, 0));
 
-    INVOKE_TASK_WHEN(&e->events.killed, common_drop_items, &e->pos, {
-        .points = 1,
-        .power = 1,
-    });
+	INVOKE_TASK_WHEN(&e->events.killed, common_drop_items, &e->pos, {
+		.points = 1,
+		.power = 1,
+	});
 
-    e->move.attraction_point = ARGS.pos + 120*I;
-    e->move.attraction = 0.03;
+	e->move.attraction_point = ARGS.pos + 120*I;
+	e->move.attraction = 0.03;
 
-    WAIT(60);
+	WAIT(60);
 
-    play_sound("shot1");
-    int n = 1.5 * global.diff - 1;
+	play_sound("shot1");
+	int n = 1.5 * global.diff - 1;
 
-    for(int i = -n; i <= n; i++) {
-        cmplx aim = cdir(carg(global.plr.pos - e->pos) + 0.2 * i);
+	for(int i = -n; i <= n; i++) {
+		cmplx aim = cdir(carg(global.plr.pos - e->pos) + 0.2 * i);
 
-        PROJECTILE(
-            .proto = pp_crystal,
-            .pos = e->pos,
-            .color = RGB(0.2, 0.3, 0.5),
-            .move = move_asymptotic_simple(aim * (2 + 0.5 * global.diff), 5),
-        );
-    }
+		PROJECTILE(
+			.proto = pp_crystal,
+			.pos = e->pos,
+			.color = RGB(0.2, 0.3, 0.5),
+			.move = move_asymptotic_simple(aim * (2 + 0.5 * global.diff), 5),
+		);
+	}
 
-    WAIT(1);
+	WAIT(1);
 
-    e->move.attraction = 0;
-    e->move.acceleration = 0.04 * ARGS.dir;
-    e->move.retention = 1;
+	e->move.attraction = 0;
+	e->move.acceleration = 0.04 * ARGS.dir;
+	e->move.retention = 1;
 
-    for(;;) {
-        YIELD;
-    }
 }
 
 // opening. projectile bursts
 TASK(burst_fairies_1, NO_ARGS) {
-    for(int i = 3; i--;) {
-        INVOKE_TASK(burst_fairy, VIEWPORT_W/2 + 70,  1 + 0.6*I);
-        INVOKE_TASK(burst_fairy, VIEWPORT_W/2 - 70, -1 + 0.6*I);
-        stage_wait(25);
-    }
+	for(int i = 3; i--;) {
+		INVOKE_TASK(burst_fairy, VIEWPORT_W/2 + 70,  1 + 0.6*I);
+		INVOKE_TASK(burst_fairy, VIEWPORT_W/2 - 70, -1 + 0.6*I);
+		WAIT(25);
+	}
 }
 
 TASK(tutorial_timeline, NO_ARGS) {
-    INVOKE_TASK_DELAYED(100, burst_fairies_1);
-
+	INVOKE_TASK_DELAYED(100, burst_fairies_1);
 
 	WAIT(1000);
 
@@ -72,7 +68,6 @@ TASK(tutorial_timeline, NO_ARGS) {
 }
 
 void tut_events(void) {
-
 	TIMER(&global.timer);
 
 	AT(0) {
