720a077a730074f2841d7fe1d88068940325183c
Alice D <alice@starwitch.productions>
2020-12-26 19:55:55 -0500

    fix kurumi.h

--- src/stages/stage4/kurumi.h
+++ src/stages/stage4/kurumi.h
@@ -16,6 +16,7 @@
 Boss *stage4_spawn_kurumi(cmplx pos);
 void kurumi_slave_visual(Enemy *e, int t, bool render);
 void kurumi_slave_static_visual(Enemy *e, int t, bool render);
+void kurumi_spell_bg(Boss*, int);
 int kurumi_splitcard(Projectile *p, int t);
 
 #endif // IGUARD_stages_stage4_kurumi_h
